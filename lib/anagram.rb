414e0022408e5e5a75813d81ce01dfd988e75516
Andrew Johnson <ajohnson.uva@gmail.com>
2018-12-03 18:51:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    memo = []
+    array.each do |anagram|
+      # binding.pry
+      memo << anagram if anagram.downcase.split("").sort.join == self.word.downcase.split("").sort.join
+    end
+    memo
+  end
+
+
+end
