d45fac572bad44f443c436edff97fb9fe66ee4dc
Henry <hanke.liu@gmail.com>
2018-12-04 18:41:05 -0500

    set initialize method and match method. All passed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
+require "pry"
 # Your code goes here!
+class Anagram
+  attr_accessor :words
+
+  def initialize(words)
+    @words = words
+
+  end
+
+  def match(anagrams)
+
+
+    anagrams.select do |anagram|
+      anagram if anagram.split("").sort == words.split("").sort
+      #binding.pry
+
+    end
+  end
+
+end #end of class
