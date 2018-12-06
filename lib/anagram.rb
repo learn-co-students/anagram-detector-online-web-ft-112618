6256015cbf252cbc833d90903f814a21b71bf957
Joe Quattrone <joequattrone5@gmail.com>
2018-12-06 11:19:52 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,13 +8,11 @@ class Anagram
   end
 
   def match(array)
-
     #array.find_all {|x| x.split("").sort == @word.split("").sort}
 
     #my solution
     word_array = array.map {|w| w if w.split("").sort == @word.split("").sort}
     word_array.compact
-
   end
 
 end
