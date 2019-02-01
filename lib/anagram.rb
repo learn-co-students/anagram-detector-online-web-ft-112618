57f131e2aa6450374b321744edcbb780d24f022c
FlipTech9 <38243324+FlipTech9@users.noreply.github.com>
2019-02-01 05:23:07 -0800

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word  
+    def initialize(word)
+        @word = word 
+
+    end 
+
+    #detect no matches, a simple anagram, an anagram, and multiple anagrams
+    def match(array)
+        array.select do |a|
+            (@word.split("").sort) == (a.split("").sort) 
+        end 
+    end 
+end 
